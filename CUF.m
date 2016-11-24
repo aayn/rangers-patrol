a6fdceed7a2ce2763d70763c6bb17e57be2d6c87
Aayush Naik <aayushnaik17@gmail.com>
2016-11-24 22:09:48 +0530

    Add output img and more variables

--- CUF.m
+++ CUF.m
@@ -1,9 +1,16 @@
-function [ u ] = CUF( o, X, gamma)
+function [ u ] = CUF(h, ad, v, alpha, beta, X, gamma)
+
+    % X is the variable set being maximized. C is now the distribution of
+    % patrollers at the nth iteration of the optimization function
+    % C -- row denotes the ith cell from top left to bottom right of
+    % rectangular grid. Column denotes the time steps .
     C = X;
     k = size(C, 2);
     i = size(C, 1);
     
     J = ones(k, k);
+    
+    % forms ones of upper right triangular matrix
     K = tril(J).';
     
     g = (1 : k);
@@ -13,11 +20,26 @@ function [ u ] = CUF( o, X, gamma)
     for i = (0 : 9)
         ga(i + 1, :) = ga(i + 1, :) / (gamma ^ i);
     end
+    
+    % K represents the decay factor matrix 
+    % 1 g g^2 g^3 ...
+    % 0 1 g   g^2 ..
+    % 0 0 1   g  ..
+    % 0 0 0   1  ...
+    % . . .   . .. 
+    % . . .   . ...
     K = K .* ga;
-    %C
+    
+    % matrix multiplying C to decay factor matrix so that future C values
+    % are less dependent on the past C values
     C = C * K;
+    
+    % summing rows to 
     s = sum(C, 2) ./ k;
-    u = s .* o;
+    
+    % behavioural and surrounding factors that determine the utility at each cell
+    all_factors = ad * alpha + v * beta + h * (1 - alpha - beta);
+    u = s .* all_factors;
     u = sum(u);
 
 end
--- plan.m
+++ plan.m
@@ -1,21 +1,65 @@
 p = rand(9, 1);
-d = [10; 2; 4; 5; 15; 18; 19; 22; 26];
-o = [23; 44; 85; 33; 49; 52; 74; 82; 71];
-gamma = 0.9;
 
-fun = @(x) 1/ CUF(o, x, gamma);% .* Ud(d, p));
+% Animal density : Animal density is a number signifying the number of
+%                  animals in a particular cell.
+% animal_den_i = density of animals in cell i.
+animal_den = [40; 18; 44; 34; 22; 36; 29; 21; 24];
+
+
+% history : This vector gives a degree corresponding to different cells
+%           based on prior poacher activity
+% history_i = degree of poacher activity in cell i.
+history = [17; 35; 39; 15; 28; 33; 36; 32; 29];
+
+
+% vulnerability : Gives an estimate on how easy it is to poach in a
+%                 particular cell.
+% vulnerabilty_i = ease of poaching in cell i.
+vulnerability = [43; 36; 11; 18; 21; 39; 4; 16; 29];
+
+% gamma : Gives an estimate of the decay factor in accurate prediction of
+%         patrol strategies.
+gamma = 0.999;
+
+% These factors give weights to contribution of animal density,
+% vulnerabiltiy and history.
+alpha = 0.3;
+beta = 0.2;
+
+% This is the optimization function for x, where x is set of patrolling
+% strategies for 10 days
+fun = @(x) 1/ CUF(history, animal_den, vulnerability, alpha, beta, x, gamma);% .* Ud(d, p));
+
+% This is the initial values of x set randomly
 x0 = rand(9, 10);
-% p1 = Ud(d, p);
+
+% A corresponds to linear inequality constraints 
+% b corresponds to number of patrollers at time instant i
 A = zeros(10, 90);
+b = [5 5 5 5 5 5 5 5 5 5].';
+
+% set indices of sparse matrix A to ones for every
+%[ 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 .....]
+%[ 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 .....]
+%[ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 .....]
+%[ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 .....]
+%[ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 .....]
+
 for i = (1 : 10)
     A(i, (9 * (i - 1)) + 1 : i * 9) = ones(1, 9);
 end
 
-b = [3 3 3 3 3 3 3 3 3 3].';
 p2 = 72;
+
+% lower bounds for linear inequalities
 lb = zeros(9, 10);
+% upper bounds for linear inequalities
 ub = ones(9, 10);
-nonlcon = [];
+
+% set of non linear constraints.
+nonlcon = []; 
 options = optimset('Largescale','off','Display','iter', 'MaxFunEvals', 100000);
+% fmincon finds the minimum of a problem specified by parameters like linear
+% inequalities , non linear inequalities, 
 [x, fval] = fmincon(@(x)fun(x), x0, A, b, [], [], lb, ub, nonlcon, options);
 
